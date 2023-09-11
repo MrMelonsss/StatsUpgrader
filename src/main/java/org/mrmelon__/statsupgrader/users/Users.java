fa1f2b0fa84a59cfad6342566b2528ce0e914fc9
PC <makcim.komissarov@mail.ru>
2023-09-11 23:33:28 +0300

    Add args of /adminsu command:  - userstats     - playtime < null | Player > Add UsersStats DB and getters of player stats Add statisticAdder in EventHandler.JoinServer

--- src/main/java/org/mrmelon__/statsupgrader/users/Users.java
+++ src/main/java/org/mrmelon__/statsupgrader/users/Users.java
@@ -7,8 +7,7 @@ public class Users {
     private static ArrayList<String> users = new ArrayList<>(); // Список всех юзеров
 
     public static boolean userChecker(String nickname){ // Проверка игрока на то, есть ли он в списке юзеров
-        if (users.contains(nickname)) return true; // Проверка на присутствие игрока в списке
-        return false;
+        return users.contains(nickname); // Проверка на присутствие игрока в списке
     }
 
     public static void addUser(String nickname){ // Метод, добавляющий юзера в список
