bbfc3a113d252774e9558c833e41a58bf10cf940
PC <makcim.komissarov@mail.ru>
2023-09-23 12:28:08 +0300

    Async was made and plugin snapshot is end.

--- src/main/java/org/mrmelon__/statsupgrader/events/EffectGiverEvent.java
+++ src/main/java/org/mrmelon__/statsupgrader/events/EffectGiverEvent.java
@@ -1,36 +1,28 @@
 package org.mrmelon__.statsupgrader.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.mrmelon__.statsupgrader.Main;
 import org.mrmelon__.statsupgrader.users.upgrades.UsersUpgrades;
 
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class EffectGiverEvent implements Listener {
 
     @EventHandler
     public void timerJoin(PlayerJoinEvent event){
-        AtomicInteger flag = new AtomicInteger();
-        flag.set(0);
-        int taskId = Bukkit.getScheduler().runTaskTimer(Main.getInstaces(),() -> {
-            try {
-                System.out.println("Func");
-                UsersUpgrades.setAllFromStats(event.getPlayer());
-                UsersUpgrades.giveLevelsEffects(event.getPlayer());
-            } catch (Exception e){
-                flag.set(1);
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                try {
+                    UsersUpgrades.setAllFromStats(event.getPlayer());
+                    UsersUpgrades.giveLevelsEffects(event.getPlayer());
+                } catch (Exception e){
+                    this.cancel();
+                }
             }
-
-            },20,20).getTaskId();
-        while (true){
-            if (flag.get()==1){
-                System.out.println("if");
-                Bukkit.getScheduler().cancelTask(taskId);
-            }
-        }
+        }.runTaskTimer(Main.getInstaces(),20L,20L);
     }
 
 }
